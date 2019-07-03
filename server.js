ef57b66189c5bbd21fddd63289b962538f762a02
Parinya Onsuwan <parinya.ons@gmail.com>
2019-07-04 00:10:36 +0700

    commit

--- server.js
+++ server.js
@@ -4,11 +4,14 @@ const fastify = require('fastify')({ logger: true })
 const authenticator = require('./plugin/authenticator')
 const errorHandler = require('./plugin/errorHandler')
 fastify
+	.register(require('fastify-cors'), { 
+		origin: true
+  	})
 	.register(fp(authenticator))
 	.register(require('./modules/scb/route'), { prefix: '/api/scb' })
 	.register(require('./modules/auth/route'), { prefix: '/api' })
 	.setErrorHandler(errorHandler)
-	.listen(3000)
+	.listen(process.env.PORT)
 	.then((address) => console.log(`server listening on ${address}`))
 	.catch(err => {
 		console.log(`Error starting server: ${err}`)
